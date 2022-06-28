9e559dd879cc02718a2a48f1f2b28c62b44eda73
JhaneAlice <jhan19cas@gmail.com>
2022-06-28 11:37:34 -0400

    feacture(common):new feacture [PC4-1]

--- src/context/Login/Login.js
+++ src/context/Login/Login.js
@@ -1,8 +1,53 @@
 import React from "react";
+import './Login.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export const Login = () => {
     return (
-        <div>
-            Login page
+        <div className="containerPrincipal">
+            <div className="containerSecundario">
+                <div className="containerTercero"><br/><br/><br/>
+                    <div className="form-group">
+                        <div className="containerT1">
+                            <label className="containerD"></label>
+                        </div>
+                        <br/>
+                        <label className="Plagio">Plagio Control</label>
+                        <br/><br/>
+                        <label className="LPlagioControl">Login Plagio Control</label><br/><br/>
+                        <label className="Enter">Enter your email and password below</label><br/><br/>
+                        <label className="Email">
+                            EMAIL
+                        </label>
+                        <br/>
+                        <input
+                            type="txt"
+                            className="form-control1"
+                            placeholder="Email addres"
+
+                        />
+                        <br/>
+                        <label className="Password">
+                            PASSWORD
+                        </label>
+                        <label >
+                           <a className="Forgot"> Forgot password?</a>
+                        </label>
+                        <br/>
+                        <input
+                            type="password"
+                            className="form-control2"
+                            placeholder="Password"
+                        />
+                        <br/>
+                        <button className="btn btn-primary">Log In</button>
+                        <br/><br/>
+                        <label className="Cuenta">No tienes una cuenta? <a className="Crea">Sing up </a></label>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
