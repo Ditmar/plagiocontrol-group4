2b9be40da9ebaa2bb9114a971c4fa37cd6862dca
Ditmar <ditmarcastro@gmail.com>
2022-06-29 09:01:44 -0400

    feature(common): draft login users in progress [pcg-101]

--- src/context/Home/Home.js
+++ src/context/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const Home = () => {
+    const [user, setUser] = userState({user: '', password: ''});
     return <div>
         Home pages
     </div>
---
+++ src/ui/components/label/label.js
@@ -0,0 +1,3 @@
+const label = () => {
+    console.log('hola')
+}
