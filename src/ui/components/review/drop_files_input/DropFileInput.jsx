b966d76876de2937b2840416650362765b40594b
bryanreyes28 <bryan28reyesirahola@gmail.com>
2022-07-03 13:37:45 -0400

    feature(common): adding the component DropFileInput's storybook[PC4-8]

--- src/ui/components/review/drop_files_input/DropFileInput.jsx
+++ src/ui/components/review/drop_files_input/DropFileInput.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 import '../../../../context/Review/review.css'
 
-import { ImageConfig } from './config/imageConfig'; 
+import { ImageConfig } from './config/imageConfig';
 
 
-import{FaTrash}from 'react-icons/fa'
+import { FaTrash } from 'react-icons/fa'
 
-const DropFileInput = props => {
+const DropFileInput = ({ isUploadedfile, props }) => {
 
     const wrapperRef = useRef(null);
 
@@ -37,54 +37,73 @@ const DropFileInput = props => {
     }
 
     return (
-        
-            <div 
-             className='dropfilesinput'
-                ref={wrapperRef}
-                onDragEnter={onDragEnter}
-                onDragLeave={onDragLeave}
-                onDrop={onDrop}
-            >
-                
-                <input
-            type="file"
-            onChange={onFileDrop}
-          />
-           {
+
+        <div
+            className='dropfilesinput'
+            ref={wrapperRef}
+            onDragEnter={onDragEnter}
+            onDragLeave={onDragLeave}
+            onDrop={onDrop}
+        >
+            {isUploadedfile ? (<div className='dropfilespreview__item'>
+                <div className="dropfilespreview__item__info">
+                    <div className="dropfilepreview__image">
+                    <img src="/pdf.png" alt="" />
+                    </div>
+                    <p className='fileName'>NOCHE DE TALENTOS  PROGRAMA  MAYO 2022.pdf </p>
+                    <br />
+                    <p className='fileSize'> 0.92MB</p>
+                    <div className="dropfilespreview__item__del">
+                        <FaTrash
+                        />
+                    </div>
+                </div>
+            </div>) : (<input
+                type="file"
+                onChange={onFileDrop}
+            />)}
+
+            {
                 fileList.length > 0 ? (
+
+
                     <div className="dropfilespreview">
                         {
                             fileList.map((item, index) => (
+
                                 <div key={index} className="dropfilespreview__item">
+
                                     <div className="dropfilespreview__item__info">
-                                        
+                                        <div className="dropfilepreview__image">
                                         <img src={ImageConfig[item.type.split('/')[1]] || ImageConfig['pdf']} alt="" />
+                                        </div>
                                         
-                                        <p>{item.name}</p>
+                                        <p className='fileName'>{item.name} </p>
+                                        <br />
+                                        <p className='fileSize'>{(item.size / 1000000).toFixed(2)}MB</p>
                                         
-                                        <p>{(item.size/1000000).toFixed(2)}MB</p>
                                         
+
                                         <div className="dropfilespreview__item__del">
-                                    <FaTrash 
-                                    onClick={() => fileRemove(item)}
-                                    />
+                                            <FaTrash onClick={() => fileRemove(item)}
+                                            />
+                                        </div>
                                     </div>
-                                    </div>
-                                
+
                                 </div>
                             ))
                         }
                     </div>
                 ) : null
             }
-            </div>
-            
-        
+        </div>
+
     );
 }
 
 DropFileInput.propTypes = {
-    onFileChange: PropTypes.func
+    onFileChange: PropTypes.func,
+    isUploadedfile: PropTypes.bool
 }
 
 export default DropFileInput;
