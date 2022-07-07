8a9dea747ba2dd3401f0cc053f418a3141b4b961
jes1ca <ycikale.77@gmail.com>
2022-07-06 20:46:19 -0400

    feacture(common):menu storybook almost completed[WEBSISTESPC-5]

--- src/ui/components/Sidebar/Sidebar.js
+++ src/ui/components/Sidebar/Sidebar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import './Sidebar.css';
 import {
     BrowserRouter,
     Routes,
     Route,
+    Link
 } from "react-router-dom";
 import Logo from "./img/logo";
 import vec1 from "./img/vec1.svg";
@@ -14,53 +16,64 @@ import vec5 from "./img/vec5.svg";
 import vec6 from "./img/vec6.svg";
 import vec7 from "./img/vec7.svg";
 import { string } from "prop-types";
+import { ListDocs } from "../../../context/listDocs/ListDocs";
+import { Works } from "../../../context/Works/Works";
+import { Home } from "../../../context/Home/Home";
+import { Review } from "../../../context/Review/Review";
+import { BurguerButton } from "./components/BurguerButton";
 
 export const Sidebar = ({tittle}) => {
-
+    const [clicked, setClicked]=useState(false);
+    const handleClick = () => {
+        setClicked(!clicked)
+    }
     return(
-      
+        <>
+        <div className="burguer" >
+            <BurguerButton/>
+        </div>      
     <div className="sidebar"> 
-     <div className="logo">
-           <Logo/>
-           </div>
-           <div className="tittle">
-           {tittle || 'Plagio Control'}
-           </div>
-            
-        <ul className="menu">
-            <li>
-                <img src={vec1}></img>
-                <a>Inicio</a>
-            </li>
-            <li>
-                <img src={vec2}></img>
-                <a>Trabajo</a>
-            </li>
-            <li>
-                <img src={vec3}></img>
-                <a>Revisar Tesis</a>
-            </li>
-            <li>
-                <img src={vec4}></img>
-                <a>Listar Tesis</a>
-            </li>
-            <li>
-                <img src={vec5}></img>
-                <a>Publicar Tesis</a>
-            </li>
-            <li>
-                <img src={vec6}></img>
-                <a>Reportes Generales</a>
-            </li>
-            <div className="divider"/>
-            <li className="settings">
-                <img src={vec7}></img>
-                <a>Settings</a>
-            </li>
-        </ul>
-            
+            <div className="logo">
+                <Logo/>
+            </div>
+            <div className="tittle">  
+                    {tittle || 'Plagio Control'}
+            </div>
+            <ul className="menu">
+                <li>
+                    <img src={vec1}></img>
+                    <a>Inicio</a>
+                </li>
+                <li>
+                    <img src={vec2}></img>
+                    <a>Trabajo</a>
+                </li>
+                <li>
+                    <img src={vec3}></img>
+                    <a>Revisar Tesis</a>
+                </li>
+                <li>
+                    <img src={vec4}></img>
+                    <a>Listar Tesis</a>
+                </li>
+                <li>
+                    <img src={vec5}></img>
+                    <a>Publicar Tesis</a>
+                </li>
+                <li>
+                    <img src={vec6}></img>
+                    <a>Reportes Generales</a>
+                </li>
+                <div className="divider"/>                    
+                <li className="settings">
+                    
+                    <img src={vec7}></img>
+                    <a>Settings</a>
+                </li>
+            </ul>
     </div>
+    </>
+ 
    
     )
 }
-
---
+++ src/ui/components/Sidebar/components/BurguerButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import bar from "../img/bar.jpeg";
+export const BurguerButton = (props) => {
+    return(
+            <div>
+                 <img src={bar} />
+            </div>
+    )
+}
