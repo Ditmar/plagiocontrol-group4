27073cbcfcd12fa13b16df5140345cff28ee4f41
J Daniel Janko Tola <baniel.1ov3@gmail.com>
2022-07-10 18:36:55 +0000

    solucionando errores version 2

--- src/App.js
+++ src/App.js
@@ -11,16 +11,4 @@ function App() {
   ); 
 } 
  
-import { Register } from './ui/components/register/Register';
-
-import Navigation from './context/Navigation';
-function App() {
-  return (
-    <div>
-       <Navigation />
-     
-    </div>
-  );
-}
-
 export default App;
--- src/context/Home/Home.js
+++ src/context/Home/Home.js
@@ -5,15 +5,4 @@ export const Home = () => {
     return <div> 
         <RecentDocuments/> 
     </div> 
-
-import React from 'react';
-export const Home = () => {
-    return (
-    <>
-    <div>
-        Home              
-    </div> 
- 
-    </>)
-
 }
